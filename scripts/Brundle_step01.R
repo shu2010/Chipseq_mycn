dbc30b0fa7c95a40a915055c8e015fbf49d96269
shu2010 <s.pattnaik@garvan.org.au>
2020-12-12 16:51:31 +1100

    workflow_script

---
+++ scripts/Brundle_step01.R
@@ -0,0 +1,39 @@
+##Brundle workflow for sharing
+##sample sheets Experiment_sample_sheet.csv and Control_sample_sheet.csv are shared on github data folder
+
+library(Brundle)
+
+##Initial conditions
+jg.controlMinOverlap      <- 5
+jg.treatedCondition       =  "DOX"
+jg.untreatedCondition     =  "none"
+
+
+##Create Diffbind object
+dbaExperiment <- jg.getDba("path/to/Experiment_sample_sheet.csv", bRemoveDuplicates=TRUE)
+dbaControl    <- jg.getDba("path/to/Control_sample_sheet.csv", bRemoveDuplicates=TRUE)
+
+saveRDS(dbaExperiment, "diffbind_exp", compress = T) ##Please share
+saveRDS(dbaControl, "diffbind_cont", compress = T) ##Please share
+
+##Read counts from peak data
+
+jg.experimentPeakset <- jg.dbaGetPeakset(dbaExperiment)
+jg.controlPeakset    <- jg.dbaGetPeakset(dbaControl)
+
+saveRDS(jg.experimentPeakset, "control_counts_expt_object", compress = T) ##Please share
+saveRDS(jg.controlPeakset, "control_counts_control_object", compress = T) ##Please share
+
+
+#Get counts for the treated control samples.
+jg.controlCountsTreated<-jg.getControlCounts(jg.controlPeakset,
+                                             jg.controlSampleSheet,
+                                             jg.treatedCondition )
+#Repeat for the untreated/control samples
+jg.controlCountsUntreated<-jg.getControlCounts(jg.controlPeakset,
+                                               jg.controlSampleSheet,
+                                               jg.untreatedCondition)
+
+
+saveRDS(jg.controlCountsTreated, "control_counts_treated_object",compress = T) ##Please share
+saveRDS(jg.controlCountsUntreated, "contro_counts_untreated_object", compress = T) ##Please share
